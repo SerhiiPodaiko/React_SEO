47ec624a1ba81ea7228dc1c1ba68a0750bb557fc
SerhiiPodaiko <podajko1997@gmail.com>
2023-10-16 14:48:11 +0300

    Updates

---
+++ 31.59782d0e24dc14372b8c.nomin.js
@@ -0,0 +1,42 @@
+"use strict";
+(self["webpackChunkproxy_seller_test_task_seo"] = self["webpackChunkproxy_seller_test_task_seo"] || []).push([[31],{
+
+/***/ 31:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _assets_icons_error_svg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(504);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(893);
+
+
+
+var ErrorPage = function ErrorPage() {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("section", {
+    className: "vh-100 p-5 rounded",
+    style: {
+      background: '#F4DEDE',
+      border: '2px solid #DA7987',
+      color: '#DA7987'
+    },
+    children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+      className: "d-flex align-items-center justify-content-between",
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h2", {
+        children: "Opps, Error"
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)(_assets_icons_error_svg__WEBPACK_IMPORTED_MODULE_0__/* ["default"] */ .Z, {})]
+    }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("button", {
+      className: "btn btn-danger",
+      onClick: function onClick() {
+        return reset();
+      },
+      children: "Try again"
+    })]
+  });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ErrorPage);
+
+/***/ })
+
+}]);
---
+++ 896.59782d0e24dc14372b8c.nomin.js
@@ -0,0 +1,191 @@
+"use strict";
+(self["webpackChunkproxy_seller_test_task_seo"] = self["webpackChunkproxy_seller_test_task_seo"] || []).push([[896],{
+
+/***/ 896:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Settings_SettingsPage)
+});
+
+// EXTERNAL MODULE: ./node_modules/react-helmet/es/Helmet.js
+var Helmet = __webpack_require__(593);
+// EXTERNAL MODULE: ./node_modules/react-router-dom/dist/index.js
+var dist = __webpack_require__(655);
+// EXTERNAL MODULE: ./node_modules/classnames/index.js
+var classnames = __webpack_require__(184);
+var classnames_default = /*#__PURE__*/__webpack_require__.n(classnames);
+;// CONCATENATED MODULE: ./src/components/Settings/Settings.module.scss
+// extracted by mini-css-extract-plugin
+/* harmony default export */ const Settings_module = ({"settings":"AbaWFnfwItP36q2gdULK","settingsTitle":"ejVPRLtKweACeDxbvqPz","settingsHead":"x21siphHNKf0plzlyf78"});
+// EXTERNAL MODULE: ./src/constants/pages.js
+var pages = __webpack_require__(348);
+;// CONCATENATED MODULE: ./src/assets/images/desktop-full.png
+/* harmony default export */ const desktop_full = (__webpack_require__.p + "35dbf5632338d3cbdbea861e3a836b35.png");
+;// CONCATENATED MODULE: ./src/assets/images/desktop-short.png
+/* harmony default export */ const desktop_short = (__webpack_require__.p + "825674a4907f2825c9a872e5b1833866.png");
+;// CONCATENATED MODULE: ./src/assets/images/laptop-full.jpg
+/* harmony default export */ const laptop_full = (__webpack_require__.p + "ddb3d28e53961183ac9a6bf6e9328e7b.jpg");
+;// CONCATENATED MODULE: ./src/assets/images/laptop-short.jpg
+/* harmony default export */ const laptop_short = (__webpack_require__.p + "92b2427c96d5702d3cb3e7f4e5242623.jpg");
+;// CONCATENATED MODULE: ./src/assets/images/mobile-full.jpg
+/* harmony default export */ const mobile_full = (__webpack_require__.p + "c8df192aa364593a25c53a11577c11af.jpg");
+;// CONCATENATED MODULE: ./src/assets/images/mobile-short.jpg
+/* harmony default export */ const mobile_short = (__webpack_require__.p + "f394fa25d2ab2fd33a4feb9d85cd0474.jpg");
+// EXTERNAL MODULE: ./node_modules/react/jsx-runtime.js
+var jsx_runtime = __webpack_require__(893);
+;// CONCATENATED MODULE: ./src/components/Settings/Settings.jsx
+
+
+
+
+
+
+
+
+
+
+
+
+var Settings = function Settings() {
+  return /*#__PURE__*/(0,jsx_runtime.jsx)("section", {
+    className: classnames_default()('p-2', Settings_module.settings),
+    children: /*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+      className: "container",
+      children: [/*#__PURE__*/(0,jsx_runtime.jsxs)("header", {
+        className: "d-flex flex-column gap-2",
+        children: [/*#__PURE__*/(0,jsx_runtime.jsx)(dist/* Link */.rU, {
+          to: pages/* PAGE_SLUGS */.k.home,
+          className: "d-flex align-self-start btn btn-outline-primary",
+          children: "Back to home"
+        }), /*#__PURE__*/(0,jsx_runtime.jsx)("h4", {
+          className: classnames_default()('mb-4', Settings_module.settingsTitle),
+          children: "Settings"
+        })]
+      }), /*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+        className: "row",
+        children: [/*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+          className: "col-12",
+          children: [/*#__PURE__*/(0,jsx_runtime.jsx)("h4", {
+            className: Settings_module.settingsHead,
+            children: "Desktop version"
+          }), /*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+            className: "pt-3 border rounded position-relative",
+            children: /*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+              className: "row",
+              children: [/*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+                className: "col-12 col-md-6",
+                children: /*#__PURE__*/(0,jsx_runtime.jsx)("img", {
+                  className: "img-fluid",
+                  src: desktop_full,
+                  alt: "Desktop Full"
+                })
+              }), /*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+                className: "col-12 col-md-6 mt-4 mt-md-0",
+                children: /*#__PURE__*/(0,jsx_runtime.jsx)("img", {
+                  className: "img-fluid",
+                  src: desktop_short,
+                  alt: "Desktop Short"
+                })
+              })]
+            })
+          })]
+        }), /*#__PURE__*/(0,jsx_runtime.jsx)("hr", {
+          className: "border-3 my-4"
+        }), /*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+          className: "col-12",
+          children: [/*#__PURE__*/(0,jsx_runtime.jsx)("h4", {
+            className: Settings_module.settingsHead,
+            children: "Laptop version"
+          }), /*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+            className: "pt-3 border rounded position-relative",
+            children: /*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+              className: "row",
+              children: [/*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+                className: "col-12 col-md-6",
+                children: /*#__PURE__*/(0,jsx_runtime.jsx)("img", {
+                  className: "img-fluid",
+                  src: laptop_full,
+                  alt: "Laptop Full"
+                })
+              }), /*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+                className: "col-12 col-md-6 mt-4 mt-md-0",
+                children: /*#__PURE__*/(0,jsx_runtime.jsx)("img", {
+                  className: "img-fluid",
+                  src: laptop_short,
+                  alt: "Laptop Short"
+                })
+              })]
+            })
+          })]
+        }), /*#__PURE__*/(0,jsx_runtime.jsx)("hr", {
+          className: "border-3 my-4"
+        }), /*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+          className: "col-12",
+          children: [/*#__PURE__*/(0,jsx_runtime.jsx)("h4", {
+            className: Settings_module.settingsHead,
+            children: "Mobile version"
+          }), /*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+            className: "pt-3 border rounded position-relative",
+            children: /*#__PURE__*/(0,jsx_runtime.jsxs)("div", {
+              className: "row",
+              children: [/*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+                className: "col-12 col-md-6 text-center",
+                children: /*#__PURE__*/(0,jsx_runtime.jsx)("img", {
+                  className: "img-fluid",
+                  src: mobile_full,
+                  alt: "Mobile Full"
+                })
+              }), /*#__PURE__*/(0,jsx_runtime.jsx)("div", {
+                className: "col-12 col-md-6 mt-4 mt-md-0 text-center",
+                children: /*#__PURE__*/(0,jsx_runtime.jsx)("img", {
+                  className: "img-fluid",
+                  src: mobile_short,
+                  alt: "Mobile Short"
+                })
+              })]
+            })
+          })]
+        })]
+      })]
+    })
+  });
+};
+/* harmony default export */ const Settings_Settings = (Settings);
+;// CONCATENATED MODULE: ./src/assets/favicons/favicon-settings.png
+/* harmony default export */ const favicon_settings = (__webpack_require__.p + "ccaeb05bd96e609cf78a3eb2e7b4b8ac.png");
+;// CONCATENATED MODULE: ./src/pages/Settings/SettingsPage.jsx
+
+
+
+
+
+
+var SettingsPage = function SettingsPage() {
+  return /*#__PURE__*/(0,jsx_runtime.jsxs)(jsx_runtime.Fragment, {
+    children: [/*#__PURE__*/(0,jsx_runtime.jsx)(Helmet/* Helmet */.q, {
+      title: "Settings",
+      meta: [{
+        'name': 'description',
+        'content': 'Settings, versions website'
+      }],
+      link: [{
+        'rel': 'icon',
+        'href': favicon_settings
+      }, {
+        'rel': 'icon',
+        'sizes': '32x32',
+        'href': favicon_settings
+      }]
+    }), /*#__PURE__*/(0,jsx_runtime.jsx)(Settings_Settings, {})]
+  });
+};
+/* harmony default export */ const Settings_SettingsPage = (SettingsPage);
+
+/***/ })
+
+}]);
