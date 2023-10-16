2fd85733eebf258fe7ff0714a6b208916da7a69f
SerhiiPodaiko <podajko1997@gmail.com>
2023-10-16 14:58:51 +0300

    Fix deploy (svg)

--- webpack.config.js
+++ webpack.config.js
@@ -80,11 +80,11 @@ module.exports = {
             },
 
             // Svg
-            {
-                test: /\.svg$/i,
-                issuer: /\.[jt]sx?$/,
-                use: ['@svgr/webpack'],
-            }
+            // {
+            //     test: /\.svg$/i,
+            //     issuer: /\.[jt]sx?$/,
+            //     use: ['@svgr/webpack'],
+            // }
         ]
     },
     resolve: {
